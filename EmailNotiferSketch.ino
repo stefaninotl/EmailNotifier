72218f791a36b54089d73e2c150eda4dadbc34e1
stefaninotl <stefano.prrn@outlook.com>
2016-04-16 17:35:52 +0200

    Added files via upload

---
+++ EmailNotiferSketch.ino
@@ -0,0 +1,27 @@
+#define LED 7 
+void setup() {
+  Serial.begin(9600);  
+  pinMode(LED, OUTPUT);
+}
+
+//Main Loop
+
+void loop() {
+
+  if (Serial.available() > 0) {
+  int comando = Serial.read(); 
+  
+    if (comando == 1)
+    {
+    digitalWrite(LED, HIGH); 
+    }
+    else
+    {
+    digitalWrite(LED, LOW); 
+    }
+    //-----------------------------------------------------------------------------
+  
+  Serial.flush();
+  delay(2000);                 
+  }
+} 
